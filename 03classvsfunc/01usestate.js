3dda7e1bfc8941c80f032dbb7f6c97bfee0838cd
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-11-19 21:35:07 -0800

    removed cra for the funcs. doing something different.

--- 03classvsfunc/01usestate.js
+++ 03classvsfunc/01usestate.js
@@ -1,2 +1,5 @@
 // NEW IDEA. Use CRA in this dir and continue inside the app with running examples.
 
+// UPDATE: The tut being followed was outdated. Had the use of: ReactDOM.render
+// This is no-longer supported.
+
