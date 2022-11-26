730153e895167d6c37b86f37c76f54bd822e0f0d
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-11-25 19:17:34 -0800

    react router basics taking shape with data retreival. more general cleanup done.

--- 05cra/mycra/src/index.jsx
+++ 05cra/mycra/src/index.jsx
@@ -1,19 +1,15 @@
 // https://reactrouter.com/en/main/start/tutorial
-// NOTE: This tut has a dsicrepancy; main.jsx did not work as an entrypoint as in the tut, but
+// NOTE: This tut has a discrepancy; main.jsx did not work as an entrypoint as in the tut, but
 // naming it to index.jsx did work. (The original index.js had to be moved aside.)
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from 'react-router-dom';
-// import './index.css';
-import './routerex/contacts.css';  // For react-router experiment
-import Root from './routes/root';
-import ErrorPage from './routes/error-page';
-import Map from './fccamp/Map';
+import { createBrowserRouter, RouterProvider, Route, } from 'react-router-dom';
+import Root, { loader as rootLoader } from './routes/root.jsx';
+import './routerex/contacts.css';
+import ErrorPage from './routes/error-page.jsx';
+import Contact from './routes/contact.jsx';
+import Map from './fccamp/Map.js';
 
 
 const router = createBrowserRouter([
@@ -21,17 +17,19 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: '/map',
     element: <Map />,
-    // errorElement: <ErrorPage />,
   },
 ]);
-// TODO: See if it is possible for errorElement to be defined on the root path alone?
-//   If it is simply a 404, then it has a global context and should only be defined once, ideally.
-//   NOTE: The ErrorPage component show one of two possible error messages so in fact, it is custom.
-//     This further supports that only one is needed.
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
