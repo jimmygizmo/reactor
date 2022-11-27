48ffe3d68f83a9ff013f3330a1a9803019dae0e7
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-11-27 15:07:39 -0800

    loading of query and setting it in box via useEffect solved the worst issues with the search with respect to navigating etc.

--- 05cra/mycra/src/routes/root.jsx
+++ 05cra/mycra/src/routes/root.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation } from 'react-router-dom';
 import { getContacts, createContact } from '../routerex/contacts.js';
 
@@ -12,17 +13,22 @@ export async function loader({ request }) {
   // ** When root first loads, q = null.  ** Hitting enter in an empty search box gives, q = '' (nothing in log).
   // This makes sense because the search Form is ONLY submitted via enter within it. BOTH values are falsy.
   // Filtering using matchsorter() is ONLY applied for a truthy q. This is the nice simple behavior we see.
-  // BUT the search box does not work perfectly in all cases. TODO: Note here how it can be improved.
+  // BUT the search box does not work perfectly in all cases. TODO: Note here how it can be improved. DONE. PARTIAL FIX.
   // 1. If you click a 'found' item, the search resets. Must search again each time you select a found item.
   //      BUT, what makes it much worse is the search terms stay in the box, yet the search has been canceled and
   //      the full contact set is shown. A partial fix would be to have the box clear, but a proper fix would
   //      be for the search results to never change based on navigation, only on a manual reset in one of two
   //      ways of the search; clear and enter OR click the x to cancel search and reset.
+  // UPDATE: The tutorial identifies two problems which cover some of the above:
+  // A. If you click back after a search, the form field still has the value you entered even though the list
+  //    is no longer filtered.
+  // B. If you refresh the page after searching, the form field no longer has the value in it,
+  //    even though the list is filtered.
   const q = url.searchParams.get("q");
   console.log('LOADER: root params q: ' + q);
   const contacts = await getContacts(q);
-  console.log('LOADER: root contacts: ' + JSON.stringify( contacts, null,2 ));
-  return { contacts };
+  console.log('LOADER: root contacts: ' + JSON.stringify( contacts, null, 2 ));
+  return { contacts, q };  // FIXED: q added to the return object so we can get the form in sync with the URL.
 }
 
 // This ACTION is imported in index.jsx, where the router and its routes are defined.
@@ -40,12 +46,21 @@ export async function action() {
 
 const Root = () => {
   const loaderData = useLoaderData();
-  // console.log(JSON.stringify(loaderData));  // loaderData is just so we could see the full object returned.
+  // console.log('useLoaderData: ' + JSON.stringify( loaderData, null, 2 ));  // loaderData is just so we could see the full object returned.
   // But really, we know this came from the loader() above and from getContacts(). The format is the same.
   // CONCLUSION: useLoaderData() does not change the data returned by loader() at all.
-  // Router calls loader() during navigation of routes.
-  const { contacts } = loaderData;  // { contacts } is just destructuring.
+  // Router calls loader() during navigation of routes; I guess this is pre-rendering.
+  //   We might say; Elements of Routes (routes) call useLoaderData when rendering.
+  const { contacts, q } = loaderData;  // { contacts, q } is just destructuring. Also, FIXED: q added for URL syncing.
   const navigation = useNavigation();
+
+  useEffect(() => {
+      // Set the value of the search input field. Runs when this Component mounts, updates (or will-unmount?)
+      // UPDATE: Adding this finally allowed the search term to remain after a full page reload, but we still have
+      // the fact the when you open a record to view and maybe edit, the search resets. Maybe this tut never addresses
+      // that issue.
+      document.getElementById("q").value = q;
+    }, [q]);
 
   return(
     <>
@@ -59,6 +74,7 @@ const Root = () => {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div
               id="search-spinner"
