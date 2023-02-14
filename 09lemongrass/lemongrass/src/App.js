421dbd1b7bd33667b91e0c5d5571b5c6b0fcd359
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2023-02-13 18:06:43 -0800

    09lemongrass - crude demo of useEffect with onClick buttons changing api call params as a lead-in to better React Router strategy which would do the same by triggering off URL paths.

---
+++ 09lemongrass/lemongrass/src/App.js
@@ -0,0 +1,23 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 09lemongrass/lemongrass/src/UseEffectRestBasic.js
@@ -0,0 +1,73 @@
+
+import React, { useState, useEffect } from 'react';
+import './index.css';
+
+// Code in this file up through this time marker in the video:
+// https://youtu.be/0ZJgIjIuY7U?list=PLZlA0Gpn_vH8EtggFGERCwMY5u5hOjf-h
+// At this marker, we move on to next file: UseEffectWindow.js
+
+
+function UseEffectRestBasic() {
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState(() => { return [] });  // Probably no benefit from anon func.
+  // Anon func might help if we were doing something expensive in initialItems or for setState() initial.
+  // const [items, setItems] = useState([]);  // No anon func
+
+  // function initialItems() {
+  //   return [];
+  // }
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+    // console.log('UseEffectRestBasic: resourceType changed')
+  }, [resourceType]);
+
+
+  return (
+    <>
+      <div className="container">
+        <h2>UseEffectRestBasic</h2>
+        <hr />
+        <br />
+        <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+        </div>
+
+        <h1>{resourceType}</h1>
+
+        { items.map((item, idx) => {
+            return <pre className="item" key={idx}>{
+              JSON.stringify(item, null, 2)}
+            </pre>
+          })
+        }
+
+      </div>
+    </>
+  );
+}
+
+export default UseEffectRestBasic;
+
+
+// ##
+// #
+//
+// https://reactjs.org/docs/hooks-effect.html
+//
+// https://jsonplaceholder.typicode.com
+//
+// EXCELLENT RESOURCE FOR FETCH USAGE VARIANTS (PROMISES):
+// https://jsonplaceholder.typicode.com/guide/
+//
+// https://www.youtube.com/watch?v=0ZJgIjIuY7U
+
+// Typicode usage example:
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
